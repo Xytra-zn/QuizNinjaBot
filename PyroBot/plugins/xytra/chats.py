e6146b0fd4a16e849db5ea51c14871a7796ae7d9
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-31 20:41:29 +0530

    Update chats.py

--- PyroBot/plugins/xytra/chats.py
+++ PyroBot/plugins/xytra/chats.py
@@ -1,21 +1,17 @@
-from pyrogram import Client
-from pymongo import MongoClient
-from config import MONGO_URL
+from config import db
 
-# Initialize MongoDB
-MONGO_URL = MONGO_URL  # Replace with your MongoDB connection string
-client = MongoClient(MONGO_URL)
-db = client["NINJAQUIZ"]  # Replace with your database name
-chatsdb = db["chatsdb"]
+chatsdb = db.chatsdb
+
 
 async def get_served_chats() -> list:
     chats = chatsdb.find({"chat_id": {"$lt": 0}})
     if not chats:
         return []
     chats_list = []
-    async for chat in chats:
+    for chat in await chats.to_list(length=1000000000):
         chats_list.append(chat)
     return chats_list
+
 
 async def is_served_chat(chat_id: int) -> bool:
     chat = await chatsdb.find_one({"chat_id": chat_id})
@@ -23,15 +19,16 @@ async def is_served_chat(chat_id: int) -> bool:
         return False
     return True
 
+
 async def add_served_chat(chat_id: int):
     is_served = await is_served_chat(chat_id)
     if is_served:
         return
     return await chatsdb.insert_one({"chat_id": chat_id})
 
+
 async def remove_served_chat(chat_id: int):
     is_served = await is_served_chat(chat_id)
     if not is_served:
         return
     return await chatsdb.delete_one({"chat_id": chat_id})
-  
