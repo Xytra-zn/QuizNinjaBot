bf0b2d27b3949bb210fff543982e29b2c207d16b
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-31 21:03:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,3 +10,7 @@ API_HASH = os.environ.get('API_HASH') or config_data['env']['API_HASH']['value']
 BOT_TOKEN = os.environ.get('BOT_TOKEN') or config_data['env']['BOT_TOKEN']['value']
 MONGO_URL = os.environ.get('MONGO_URL') or config_data['env']['MONGO_URL']['value']
 OWNER_ID = os.environ.get('OWNER_ID') or config_data['env']['OWNER_ID']['value']
+
+
+mongo = MongoCli(config.MONGO_URL)
+db = mongo.Anonymous
