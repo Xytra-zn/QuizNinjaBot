447dd50cbbf3ffe69fea43049ca7de480de1c89b
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-31 14:43:33 +0530

    Update alive.py

--- PyroBot/plugins/alive.py
+++ PyroBot/plugins/alive.py
@@ -8,18 +8,17 @@ async def alive_command(bot, message):
     user = message.from_user
     chat_id = message.chat.id
 
-    # Sending the alive message with ping
+    # Sending the alive message with response time
     text = f"Hᴇʟʟᴏ ᴛʜᴇʀᴇ {user.mention}!\n"\
-           f"Yᴏᴜʀ ʙᴇʟᴏᴠᴇᴅ Qᴜɪᴢ NɪɴJᴀ ɪs ʜᴇʀᴇ, ᴡɪᴛʜ ᴘɪɴɢ ʀᴀᴛᴇ ᴏғ\n"\
+           f"Yᴏᴜʀ ʙᴇʟᴏᴠᴇᴅ Qᴜɪᴢ NɪɴJᴀ ɪs ʜᴇʀᴇ, ᴡɪᴛʜ ʀᴇsᴘᴏɴsᴇ ᴛɪᴍᴇ ᴏғ\n"\
            f"➥ {round((time.time() - start_time) * 1000, 2)} ᴍs"
 
     # Inline buttons
     keyboard = InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("➕ 𝗔𝗗𝗗 𝗜𝗡 𝗬𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣", url="https://t.me/QuizNinjaBot?startgroup=true")],
+            [InlineKeyboardButton("➕ 𝗔𝗗𝗗 𝗜𝗡 𝗬𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣", switch_inline_query="")],
             [InlineKeyboardButton("🆘 Sᴜᴘᴘᴏʀᴛ", url="https://t.me/codder_chat")],
         ]
     )
 
     await bot.send_message(chat_id, text, reply_markup=keyboard)
-  
