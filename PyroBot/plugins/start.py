14794d10e537e97bc867bf129b7b982fd31a0cda
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-31 12:30:56 +0530

    Update start.py

--- PyroBot/plugins/start.py
+++ PyroBot/plugins/start.py
@@ -1,11 +1,24 @@
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command(["start"]))
 async def start_command(bot, message):
     user = message.from_user
-    text = f"Hello {user.mention}! Welcome to Quiz Ninja Bot. 🚀\n\n"\
-           "I'm here to make learning fun through interactive quizzes. "\
-           "Type /quiz to get started with a quiz!"
+    bot_name = "YourBotName"  # Replace with your bot's name
 
-    await message.reply_text(text)
+    # Welcome message with inline buttons
+    text = f"Hᴇʏ ᴛʜᴇʀᴇ {user.first_name} ❤️\n"\
+           f"☆ Mʏsᴇʟғ 𝗤𝗨𝗜𝗭 𝗡𝗜𝗡𝗝𝗔. I ᴀᴍ ᴀɴ Mᴏᴅᴇʀɴ ɪɴᴛᴇʟʟɪɢᴇɴᴄᴇ ʙᴀsᴇᴅ 𝚀𝚄𝙸𝚉 𝙿𝙾𝙻𝙻 𝙱𝚂ᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴛᴏ sᴇɴᴅ ᴛʜᴇ ǫᴜɪᴢᴢᴇs ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘꜱ ʙᴀsᴇᴅ ᴏɴ ᴘʀᴇғᴇʀᴇɴᴄᴇs !! ☆\n\n"\
+           "✺ Usᴇ /help ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ɢᴇᴛ ᴛʜᴇ ʟɪsᴛ ᴏғ ᴍʏ ғᴇᴀᴛᴜʀᴇs ᴀɴᴅ ᴜsᴇ /setup ᴛᴏ ꜱᴇᴛᴜᴘ ᴍᴇ. ✺\n\n"\
+           "⊶ ᴜsᴇ ᴛʜᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ғᴏʀ ᴍᴏʀᴇ !"
 
+    # Inline buttons
+    keyboard = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("➕𝗔𝗗𝗗 𝗠𝗘 𝗜𝗡 𝗬𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣", switch_inline_query="")],
+            [InlineKeyboardButton("📢 CHANNEL", url="https://t.me/CODERS_ZONE"),
+             InlineKeyboardButton("🆘 SUPPORT", url="https://t.me/codder_chat")],
+        ]
+    )
+
+    await message.reply_text(text, reply_markup=keyboard)
