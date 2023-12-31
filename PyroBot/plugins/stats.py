3f3d37b8a9ffc970508df99ab2acfc6294b9f67f
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-31 15:31:02 +0530

    Update stats.py

--- PyroBot/plugins/stats.py
+++ PyroBot/plugins/stats.py
@@ -1,50 +1,25 @@
-import os
-import motor.motor_asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.dispatcher import FSMContext
-from aiogram.types import ParseMode
-from aiogram.utils import executor
-from motor.motor_asyncio import AsyncIOMotorClient
+import logging
 import asyncio
+from pyrogram import Client, filters
+from motor.motor_asyncio import AsyncIOMotorClient
 
-connection_string = "mongodb+srv://Mrdaxx123:Mrdaxx123@cluster0.q1da65h.mongodb.net/?retryWrites=true&w=majority"
-db = AsyncIOMotorClient(connection_string).db_name
-bot = Bot(token="6920434282:AAE0tBg8b8K1XRp3cWTzoqJdIG5I4imASJg", parse_mode=ParseMode.HTML)
-bot.db = db
+# Database setup
+MONGO_URL = "mongodb+srv://Mrdaxx123:Mrdaxx123@cluster0.q1da65h.mongodb.net/?retryWrites=true&w=majority"
+MONGO_CLIENT = AsyncIOMotorClient(MONGO_URL)
+MONGO_DB = MONGO_CLIENT['stats']
 
-class MyBot(Bot):
-    async def on_message(self, message: types.Message):
-        await register_user(self.db, message.from_user.id, message.from_user.username, message.from_user.first_name, message.from_user.last_name)
-        await register_chat(self.db, message.chat.id, message.chat.title)
-        return await super().on_message(message)
+# Define client instance
+app = Client("stats", "API_ID", "API_HASH")
 
-    async def on_chat_member(self, chat_member: types.ChatMemberUpdated):
-        await register_user(self.db, chat_member.new_chat_member.user.id, chat_member.new_chat_member.user.username, chat_member.new_chat_member.user.first_name, chat_member.new_chat_member.user.last_name)
-        await register_chat(self.db, chat_member.chat.id, chat_member.chat.title)
-        return await super().on_chat_member(chat_member)
+async def add_user(client, message):
+    user_id = message.from_user.id
+    user_name = message.from_user.username
+    
+    await MONGO_DB.users.insert_one({'id': user_id, 'name': user_name})
 
-async def register_user(db, user_id, username, first_name, last_name):
-    users = db["users"]
-    user = await users.find_one({"id": user_id})
-    if not user:
-        await users.insert_one({
-            "id": user_id,
-            "username": username,
-            "first_name": first_name,
-            "last_name": last_name
-        })
-
-async def register_chat(db, chat_id, title):
-    chats = db["chats"]
-    chat = await chats.find_one({"id": chat_id})
-    if not chat:
-        await chats.insert_one({
-            "id": chat_id,
-            "title": title
-        })
-
-def main():
-    from aiogram.utils import executor
-    executor.start_polling(bot, skip_updates=True)
-
+@app.on_message(filters.command("stats") & filters.chat(chat_id=config('GROUP_ID')))
+async def get_stats(client, message):
+    total_users = await MONGO_DB.users.count_documents({})
+    total_chats = await MONGO_DB.chats.count_documents({})
+    
+    await message.reply_text(f"Total Users: {total_users}\nTotal Chats: {total_chats}")
