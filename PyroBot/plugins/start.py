b109bba6e0b7d9a1c5018fabbab1efb586e02166
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-31 12:56:08 +0530

    Update start.py

--- PyroBot/plugins/start.py
+++ PyroBot/plugins/start.py
@@ -16,10 +16,30 @@ async def start_command(bot, message):
     keyboard = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("➕𝗔𝗗𝗗 𝗠𝗘 𝗜𝗡 𝗬𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣", url="http://t.me/QuizNinjaBot?startgroup=true")],
-            [InlineKeyboardButton("❓ HELP", callback_data="help_button")],
+            [InlineKeyboardButton("❓ HELP", callback_data="help_comand")],
             [InlineKeyboardButton("📢 CHANNEL", url="https://t.me/CODERS_ZONE"),
              InlineKeyboardButton("🆘 SUPPORT", url="https://t.me/codder_chat")],
         ]
     )
 
     await message.reply_text(text, reply_markup=keyboard)
+
+
+async def help_comand(bot, message):
+    # Help message with setup instructions
+    help_text = "🔧 **Setup Instructions:**\n"\
+                "1. Add me to your group.\n"\
+                "2. Use the `/setup` command to set up your group's preferences.\n"\
+                "3. Choose class, stream, and exam focus to personalize quizzes.\n\n"\
+                "✅ That's it! Your group is configured for interactive quizzes."
+
+    # Inline buttons
+    keyboard = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("➕𝗔𝗗𝗗 𝗠𝗘 𝗜𝗡 𝗬𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣", switch_inline_query="")],
+            [InlineKeyboardButton("📢 CHANNEL", url="https://t.me/CODERS_ZONE"),
+             InlineKeyboardButton("🆘 SUPPORT", url="https://t.me/codder_chat")],
+        ]
+    )
+
+    await message.reply_text(help_text, reply_markup=keyboard)
