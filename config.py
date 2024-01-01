756f1152c2e6bb5938aaa6817d0d06ee474e52e8
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-01-01 15:23:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,10 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN') or config_data['env']['BOT_TOKEN']['valu
 MONGO_URL = os.environ.get('MONGO_URL') or config_data['env']['MONGO_URL']['value']
 OWNER_ID = os.environ.get('OWNER_ID') or config_data['env']['OWNER_ID']['value']
 
+# Additional chat ID variables
+CLASS_11 = list(map(int, os.environ.get('CLASS_11', '').split(','))) or config_data['env']['CLASS_11']['value'].split(',')
+CLASS_12 = list(map(int, os.environ.get('CLASS_12', '').split(','))) or config_data['env']['CLASS_12']['value'].split(',')
+CLASS_11_12 = list(map(int, os.environ.get('CLASS_11_12', '').split(','))) or config_data['env']['CLASS_11_12']['value'].split(',')
 
 mongo = MongoCli(MONGO_URL)
 db = mongo.Anonymous
