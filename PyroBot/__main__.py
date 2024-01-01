a9f009d948bf5ff1830e1e67e5780b85dde05254
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-01-01 23:52:38 +0530

    Update __main__.py

--- PyroBot/__main__.py
+++ PyroBot/__main__.py
@@ -1,16 +1,12 @@
 import os
 import logging
 import pyrogram
-import threading
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import API_ID, API_HASH, BOT_TOKEN, MONGO_URL, OWNER_ID
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-while threading.active_count() > 1:
-    time.sleep(5)
-mythread.start()
 
 if __name__ == "__main__":
     print("Starting Bot...")
