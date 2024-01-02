7eac388878bddcf6b21e8eb43056552b7f29c2e7
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-01-02 10:05:01 +0530

    Update setup.py

--- PyroBot/plugins/setup.py
+++ PyroBot/plugins/setup.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+from pyrogram import enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import CLASS_11, CLASS_12, CLASS_11_12, OWNER_ID
 
@@ -12,38 +13,42 @@ async def setup_command(bot, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
 
-    # Check if the user is an administrator of the group
+    # Check if the user is an owner or administrator of the group
     chat_member = await bot.get_chat_member(chat_id, user_id)
-    if not chat_member.status in ["administrator", "creator"]:
-        await message.reply_text("You need to be an administrator to use this command.")
-        return
+    if chat_member.status in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
+        
+        # Check if the chat is already configured
+        if chat_id in CLASS_11 or chat_id in CLASS_12 or chat_id in CLASS_11_12:
+            class_text = "Class: "
+            if chat_id in CLASS_11:
+                class_text += "11"
+            elif chat_id in CLASS_12:
+                class_text += "12"
+            elif chat_id in CLASS_11_12:
+                class_text += "11+12"
+            else:
+                class_text += "None selected"
 
-    # Check if the chat is already configured
-    if chat_id in CLASS_11 or chat_id in CLASS_12 or chat_id in CLASS_11_12:
-        class_text = "Class: "
-        if chat_id in CLASS_11:
-            class_text += "11"
-        elif chat_id in CLASS_12:
-            class_text += "12"
-        elif chat_id in CLASS_11_12:
-            class_text += "11+12"
+            await message.reply_text(
+                f"CHAT: {message.chat.title}\n{class_text}\n\n"
+                "To select the preferred class for the quizzes in this chat, just click on the buttons below."
+            )
+            return
+
+        # Check the condition in your database (replace 'mycol' with your actual collection name)
+        if mycol.find_one({'chat_id': chat_id, 'event_id': 'on'}):
+            # Send a message with inline buttons to choose the class
+            markup = InlineKeyboardMarkup([
+                [InlineKeyboardButton("11", callback_data="class_11")],
+                [InlineKeyboardButton("12", callback_data="class_12")],
+                [InlineKeyboardButton("11+12", callback_data="class_11_12")],
+            ])
+
+            await bot.send_message(chat_id, f"CHAT: {message.chat.title}\nClass: None selected\n\nTo select the preferred class for the quizzes in this chat, just click on the buttons below.", reply_markup=markup)
         else:
-            class_text += "None selected"
-
-        await message.reply_text(
-            f"CHAT: {message.chat.title}\n{class_text}\n\n"
-            "To select the preferred class for the quizzes in this chat, just click on the buttons below."
-        )
-        return
-
-    # Send a message with inline buttons to choose the class
-    markup = InlineKeyboardMarkup([
-        [InlineKeyboardButton("11", callback_data="class_11")],
-        [InlineKeyboardButton("12", callback_data="class_12")],
-        [InlineKeyboardButton("11+12", callback_data="class_11_12")],
-    ])
-
-    await bot.send_message(chat_id, "CHAT: {}\nClass: None selected\n\nTo select the preferred class for the quizzes in this chat, just click on the buttons below.".format(message.chat.title), reply_markup=markup)
+            await message.reply_text("The condition in the database is not met.")
+    else:
+        await message.reply_text("You need to be an owner or administrator to use this command.")
 
 @Client.on_callback_query()
 async def callback_handler(bot, callback_query):
